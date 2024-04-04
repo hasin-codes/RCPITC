da6c19fe5ddc2b266673679a59dd574d5612e9d2
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-04-05 01:41:10 +0600

    fix

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -6,31 +6,17 @@ const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          href="#about-us"
-          className="h-auto w-auto flex flex-row items-center"
-        >
+        {/* "RCPITC" text */}
+        <span className="font-bold ml-[10px] sm:ml-0 text-gray-300">RCPITC</span>
 
-
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-          RCPITC
-          </span>
-        </a>
-
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-us" className="cursor-pointer">
-              About us
-            </a>
-            <a href="#Explore" className="cursor-pointer">
-            Explore
-            </a>
-            <a href="#Upcoming" className="cursor-pointer">
-            Upcoming
-            </a>
-          </div>
+        {/* Navigation links */}
+        <div className="hidden sm:flex items-center justify-between w-[500px] h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <a href="#about-us" className="cursor-pointer">About us</a>
+          <a href="#Explore" className="cursor-pointer">Explore</a>
+          <a href="#Upcoming" className="cursor-pointer">Upcoming</a>
         </div>
 
+        {/* Social icons */}
         <div className="flex flex-row gap-5">
           {Socials.map((social) => (
             <Image
